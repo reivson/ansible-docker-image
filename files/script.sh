322e5d198537238e30cbc72444938937e939007c
reivson <reivsonlopes@gmail.com>
2020-04-13 14:08:54 -0300

    script atualizado

--- files/script.sh
+++ files/script.sh
@@ -1,6 +1,3 @@
-# Garantindo os hosts
- HOSTS=$(head -n7 /etc/hosts)
- echo -e "$HOSTS" > /etc/hosts
- echo '10.100.35.10 ansible-server.reivs.example' >> /etc/hosts
- echo '10.100.35.20 webserver-01.reivs.example' >> /etc/hosts
- echo '10.100.35.30 webserver-02.reivs.example ' >> /etc/hosts
+echo '10.100.35.10 ansible-server.reivs.example' >> /etc/hosts
+echo '10.100.35.20 webserver-01.reivs.example' >> /etc/hosts
+echo '10.100.35.30 webserver-02.reivs.example ' >> /etc/hosts
